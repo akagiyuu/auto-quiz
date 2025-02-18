2034c8a4fe4dd86853795c7e479e0b87f91f853d
akagiyuu <khanghuynh0245@gmail.com>
2025-02-18 12:11:25 +0700

    refactor: remove gpt and paml

--- src/answer_provider/mod.rs
+++ src/answer_provider/mod.rs
@@ -1,9 +1,2 @@
-// pub mod gpt;
 pub mod random;
-pub mod palm;
 pub mod ollama;
-use anyhow::Result;
-
-pub trait AnswerProvider {
-    async fn get_answer(&self, question: &str, possible_answers: &[String]) -> Result<usize>;
-}
