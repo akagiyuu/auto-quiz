2de2d7b5199307c11e2bf7bf849385fda71d8e4f
akagiyuu <khanghuynh0245@gmail.com>
2025-02-18 12:13:38 +0700

    refactor: adapt random provider

--- src/answer_provider/random.rs
+++ src/answer_provider/random.rs
@@ -1,13 +1,7 @@
 use rand::Rng;
 
-use super::AnswerProvider;
 use anyhow::Result;
 
-#[derive(Default)]
-pub struct Random;
-
-impl AnswerProvider for Random {
-    async fn get_answer(&self, _question: &str, possible_answers: &[String]) -> Result<usize> {
-        Ok(rand::thread_rng().gen_range(0..possible_answers.len()))
-    }
+pub async fn get_answer(_question: &str, possible_answers: &[String]) -> Result<usize> {
+    Ok(rand::thread_rng().gen_range(0..possible_answers.len()))
 }
